eba58fc96b0e2d9cab90a8c071427b4add71ff6d
Nipun Verma <Nipunverma39@gmail.com>
2022-04-19 01:53:33 +0530

    updated reactDOM.render() with reactDOM.createRoot()

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const server = http.createServer(app);
+const cors = require('cors');
 
+app.use(cors({origin:'*'}))
 app.use(express.json({strict: true}));
 
 app.use(require("./controllers"))
